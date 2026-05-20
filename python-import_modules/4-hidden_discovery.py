dd0defd0e5e23d5b795b0ee9affb176492640248
BrigidAn <ochimerema@gmail.com>
2026-05-20 10:36:07 +0200

    Who are you

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    names = dir(hidden_4)
-    for name in sorted(names):
-        if not name.startswith("__"):
+
+    for name in dir(hidden_4):
+        if name[:2] != "__":
             print(name)
