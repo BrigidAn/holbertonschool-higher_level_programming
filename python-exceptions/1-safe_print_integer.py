1d56abf4c0ad8d6bc02a5b0fb8f03c68bcce41b0
BrigidAn <ochimerema@gmail.com>
2026-05-25 09:41:38 +0200

    Safe printing of an integer

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """
+    Prints an integer with "{:d}".format()
+
+    Returns:
+        True if value is an integer and printed correctly
+        False otherwise
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
