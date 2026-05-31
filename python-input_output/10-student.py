b79c0976c07e504ce08e280855179057ba4bd16b
BrigidAn <ochimerema@gmail.com>
2026-05-31 22:06:49 +0200

    input output

---
+++ python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Student class"""
+
+
+class Student:
+    """Defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Return dictionary representation of Student
+
+        If attrs is a list of strings, only include those attributes.
+        Otherwise return all attributes.
+        """
+        if isinstance(attrs, list) and all(isinstance(i, str) for i in attrs):
+            return {key: self.__dict__[key]
+                    for key in attrs
+                    if key in self.__dict__}
+        return self.__dict__
