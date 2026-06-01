59f3be79efc3b27af1a91dbcb1f6b93d862d9135
BrigidAn <ochimerema@gmail.com>
2026-06-01 12:04:55 +0200

    Sql introduction

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,5 @@
-create table IF NOT EXISTS first_table (
-    id int, 
-    name VARCHAR(256) ,
+-- Create table first_table in the current database
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
 );
