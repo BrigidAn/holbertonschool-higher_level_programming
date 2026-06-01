f474d3910a8cef80be365a17fdc8c9dec25b5e9b
BrigidAn <ochimerema@gmail.com>
2026-06-01 15:36:35 +0200

    Object-relational mapping

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Lists all cities of a given state"""
+"""
+Lists all states from the database hbtn_0e_0_usa.
+"""
 
 import MySQLdb
 import sys
@@ -15,19 +17,10 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    query = """
-        SELECT cities.name
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
-        ORDER BY cities.id ASC
-    """
-
-    cur.execute(query, (sys.argv[4],))
-
-    cities = [row[0] for row in cur.fetchall()]
-    print(", ".join(cities))
+    for row in cur.fetchall():
+        print(row)
 
     cur.close()
     db.close()
