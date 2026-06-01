6b4b47009b841ec08b0e47c6e8a21f0ac29f54a4
BrigidAn <ochimerema@gmail.com>
2026-06-01 20:44:38 +0200

    object mapping

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
-"""
-Lists all states from the database hbtn_0e_0_usa.
-"""
-
+"""List all cities of a state - SQL injection free"""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=database
     )
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor = db.cursor()
+    cursor.execute("""
+        SELECT cities.name FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+    """, (state_name,))
 
-    for row in cur.fetchall():
-        print(row)
+    rows = cursor.fetchall()
+    print(", ".join(row[0] for row in rows))
 
-    cur.close()
+    cursor.close()
     db.close()
