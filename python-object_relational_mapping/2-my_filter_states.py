344729507b769d3b7d440302803823f358c70fe1
BrigidAn <ochimerema@gmail.com>
2026-06-01 20:20:57 +0200

    Object

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-"""Lists all states starting with N."""
-
+"""Filter states by user input"""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=database
     )
 
-    cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states "
-        "WHERE name LIKE BINARY 'N%' "
-        "ORDER BY id ASC"
-    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = '{}' \
+ORDER BY states.id ASC".format(state_name))
 
-    for row in cur.fetchall():
+    for row in cursor.fetchall():
         print(row)
 
-    cur.close()
+    cursor.close()
     db.close()
